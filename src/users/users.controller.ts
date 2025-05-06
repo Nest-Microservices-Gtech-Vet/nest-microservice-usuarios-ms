c24d485db652a2a607f4f2ed11e4e2bde3102fc6
sdqguallichicogt <sdq.guallichico@gmail.com>
2025-05-06 00:51:14 -0500

    crud funcional usuarios-ms

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,93 +13,45 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   //@Post()
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('SUPERADMIN')
+
   @MessagePattern({ cmd: 'create_users' })
-  create(@Payload() data: any) {
-    return this.usersService.create(data.createUserDto);
+  create(@Payload() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
   }
 
   //Get()F
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('SUPERADMIN')
-  @MessagePattern({ cmd: 'findAll_users' })
-  findAll(@Payload() data: any) {
-    console.log('🛠 Token recibido en usuarios-ms:', data.user);
 
-    if (!data.user) {
-      throw new Error('🚫 No se recibió el usuario autenticado');
-    }
-    return this.usersService.findAll(data.paginationDto);
+  @MessagePattern({ cmd: 'findAll_users' })
+  findAll(@Payload() paginationDto: PaginationDto) {
+    return this.usersService.findAll(paginationDto);
   }
 
   //@Get(':id')
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('SUPERADMIN')
+
   @MessagePattern({ cmd: 'findOne_users' })
-async findOne(@Payload() data: { usua_id: number }) {
-  console.log('📩 [usuarios-ms] Recibida petición para buscar usuario:', data);
+  async findOne(@Payload('id', ParseIntPipe) usua_id: number) {
+    return this.usersService.findOne(usua_id)
 
-  if (!data.usua_id) {
-    console.error('🚫 [usuarios-ms] ERROR: No se proporcionó el ID del usuario');
-    throw new RpcException({
-      message: '🚫 No se proporcionó el ID del usuario',
-      status: HttpStatus.BAD_REQUEST,
-    });
   }
 
-  try {
-    console.log(`🔍 [usuarios-ms] Buscando usuario con ID: ${data.usua_id} en la base de datos...`);
 
-    const user = await this.usersService.findOne(data.usua_id);
-
-    if (!user) {
-      console.error(`🚨 [usuarios-ms] Usuario con ID ${data.usua_id} no encontrado en la BD`);
-      throw new RpcException({
-        message: `Usuario con ID ${data.usua_id} no encontrado`,
-        status: HttpStatus.BAD_REQUEST,
-      });
-    }
-
-    console.log(`✅ [usuarios-ms] Usuario encontrado y retornado:`, user);
-    return user;
-
-  } catch (error) {
-    console.error('❌ [usuarios-ms] ERROR en findOne_users:', error);
-    throw new RpcException({
-      message: '❌ Error interno en usuarios-ms',
-      status: HttpStatus.INTERNAL_SERVER_ERROR,
-    });
-  }
-}
-
-  
 
 
 
 
   //@Patch(':id')
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('SUPERADMIN')
+
   @MessagePattern({ cmd: 'update_users' })
   update(
-    @Payload() data: any
+    @Payload() updateUserDto: UpdateUserDto
   ) {
-    console.log('🛠 Datos recibidos en usuarios-ms:', data);
+   
 
-    if (!data || !data.usua_id || !data.updateUserDto) {
-      throw new RpcException({
-        message: 'Datos inválidos para actualizar el usuario',
-        status: HttpStatus.BAD_REQUEST,
-      });
-    }
-
-    return this.usersService.update(data.usua_id, data.updateUserDto);
+    return this.usersService.update(updateUserDto.usua_id, updateUserDto);
   }
 
   //@Delete(':id')
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('SUPERADMIN')
+
   @MessagePattern({ cmd: 'delete_users' })
   remove(@Payload('usua_id', ParseIntPipe) usua_id: number) {
     const ue = usua_id;
